98766aeebe871a29684104a4400c6aa07d3f4e7d
Jose Maria Nunez Izu <jmnunezizu@gmail.com>
2014-03-17 22:46:39 +0000

    Now the youtube template gets the playlistId from the controller.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express();
 var dependencies = require('../resources/dependencies');
+var config = require('./config');
 
 function view(viewName) {
     return function(req, res) {
@@ -11,6 +12,8 @@ function view(viewName) {
 router.get('/', function(req, res) {
     res.render('index', { dependencies: dependencies });
 });
-router.get('/youtube', view('youtube'));
+router.get('/youtube', function(req, res) {
+    res.render('youtube', { playlistId: config.get('server:youtube:playlistId')})
+});
 
 module.exports = router;
